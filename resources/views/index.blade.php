dec9b3ec3f47737211a9081cd8cac693f45d704b
Edmilson Rocha <edmilsonrobson@gmail.com>
2017-08-27 15:02:50 -0300

    Basic front page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,23 @@
 @extends('layouts.app')
 @section('content')
-    <section class="section">
-        <div class="container">
-            <h1 class="title">
-                Hello World
-            </h1>
-            <p class="subtitle">
-                My first website with <strong>Bulma</strong>!
-            </p>
+    <section class="hero is-primary">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Laravel+Vue Task List
+                </h1>
+                <h2 class="subtitle">
+                    Powered by <a href="vuejs.org/v2/guide/"></a><span class="has-text-success">Vue.js 2</span></a>,
+                    crafted by <a href="https://github.com/edmilsonrobson"><span class="has-text-warning">Edmilson Rocha</span></a>
+                    with <span class="has-text-danger">♥</span>
+                </h2>
+            </div>
         </div>
     </section>
+
+    <div class="container">
+        <div class="section">
+            <todo-list></todo-list>
+        </div>
+    </div>
 @endsection
